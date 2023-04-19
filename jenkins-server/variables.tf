eaf94a4631b2e444677e2cac1c1e0deaf3206a9d
iamemmybravo@gmail.com <odauduemmanuel@nvit.tech>
2023-04-19 18:51:08 +0100

    todo

---
+++ jenkins-server/variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  default = "jenkins1"
+}
+
+variable "ssh_key_name" {
+  default = "demokp"
+}
+
+variable "ssh_private_key_path" {
+  default = "~/my-project/cw-todo-app/jenkins-server"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  default = "jenkins-project"
+}
+
+variable "ssh_key_name" {
+  default = "demokp"
+}
+
+variable "names" {
+  default = ["postgresql", "nodejs", "react"]
+}
